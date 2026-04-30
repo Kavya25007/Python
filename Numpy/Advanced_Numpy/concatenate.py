b9cd3cfa05baf635f9f8a4def94c193af36e5b41
Kavya  Kushwaha <kushwahakavya37@gmail.com>
2026-04-30 11:33:52 +0530

    concatenate

---
+++ Numpy/Advanced_Numpy/concatenate.py
@@ -0,0 +1,23 @@
+"""
+concatenate = Used to join arrays together
+np.concatenate((array1, array2), axis=0)
+
+axis = 0 → ↓ (downward, rows)
+axis = 1 → → (sideways, columns)
+
+"""
+import numpy as np
+
+a = np.array([1, 2])
+b = np.array([3, 4])
+
+c = np.concatenate((a, b))
+print(c)
+
+
+
+a = np.array([[1, 2]])
+b = np.array([[3, 4]])
+
+c1 = np.concatenate((a, b), axis=1)
+print(c1)
